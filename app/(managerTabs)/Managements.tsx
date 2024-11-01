4b5e46801db4c3b34f6ff968addb11dad3c393c0
ICaoVy <icaovy2001@gmail.com>
2024-11-01 22:37:34 +0700

    update management layout screen

--- app/(managerTabs)/Managements.tsx
+++ app/(managerTabs)/Managements.tsx
@@ -1,15 +1,10 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Href, router } from "expo-router";
 
 const Management = () => {
   return (
     <View>
       <Text>Managements</Text>
-      <Button
-        onPress={() => router.push("/ManagementScreens/ManageStaff")}
-        title="Go to Manage Staff"
-      ></Button>
     </View>
   );
 };
